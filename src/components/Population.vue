a18ff69f18638a281f527a6ca6c2e477672a65af
nilamroychoudhury <hlwmydear@gmail.com>
2021-06-16 07:39:37 +0530

    my project

---
+++ src/components/Population.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <canvas id="population-chart"></canvas>
+  </div>
+</template>
+<script>
+import Chart from 'chart.js'
+import populationData from '../population.js'
+
+export default {
+  name: 'Population',
+  data() {
+    return {
+      populationData: populationData
+    }
+  },
+  mounted() {
+    const ctx = document.getElementById('population-chart');
+    new Chart(ctx, this.populationData);
+  }
+}
+</script>
---
+++ src/population.js
@@ -0,0 +1,18 @@
+export const populationData = {
+    type: "bar",
+    data: {
+        labels: ["assam", "mumbai", "west bengal", "manipur"],
+
+        datasets: [{
+            label: "indian population",
+            type: "bar",
+            data: [88, 78, 89, 66],
+            backgroundColor: ['black', 'red', 'green', 'orange', 'blue'],
+            borderColor: "#47b784",
+            borderWidth: 3
+        }]
+    },
+
+};
+
+export default populationData;
