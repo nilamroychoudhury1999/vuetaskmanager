8029530f6342f4b47b2fced3e00200268217a2c2
Nilamroychoudhury1999 <63612266+nilamroychoudhury1999@users.noreply.github.com>
2021-06-17 21:26:17 +0530

    Update Button.vue

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,8 +3,6 @@
     class="btn">{{ text}}</button>
 </template>
 <script>
-
-
 export default{
    name:'Button',
    props:{
@@ -18,3 +16,25 @@ export default{
    }
 }
 </script>
+<style scoped>
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+</style>
