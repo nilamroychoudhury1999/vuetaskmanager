5edf20f430a7e7f0246d3400a3e5bd51268384c5
Nilamroychoudhury1999 <63612266+nilamroychoudhury1999@users.noreply.github.com>
2021-06-17 21:28:01 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -66,6 +66,7 @@ export default {
   margin: 0;
   padding: 0;
 }
+
 body {
   font-family: 'Poppins', sans-serif;
 }
@@ -78,27 +79,6 @@ body {
   padding: 30px;
   border-radius: 5px;
 }
-.btn {
-  display: inline-block;
-  background: #000;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  margin: 5px;
-  border-radius: 5px;
-  cursor: pointer;
-  text-decoration: none;
-  font-size: 15px;
-  font-family: inherit;
-}
-.btn:focus {
-  outline: none;
-}
-.btn:active {
-  transform: scale(0.98);
-}
-.btn-block {
-  display: block;
-  width: 100%;
-}
+
 </style>
+
